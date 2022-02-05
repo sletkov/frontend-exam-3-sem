44c933f5e57becd5f62b0c1f53bb9f3656dec880
sletkov <70477360+sletkov@users.noreply.github.com>
2022-02-05 19:45:25 +0300

    animals page done

---
+++ components/form.vue
@@ -0,0 +1,33 @@
+<template>
+    <form action="" v-if="formFields">
+        <div v-for="(field, index) in formFields" :key="index" class="form-group">
+            <label class="form-label">{{field.title}}</label>
+            <input  v-if="field.type === 'string'" type="text" class="form-control">
+            <select  v-else  class="form-control form-select form-select-lg mb-3">
+                <option :value="value" v-for="(value,index) in field.values " :key="index">
+                    {{value}}
+                </option>
+            </select>
+        </div>
+        <button  type="button" class="btn btn-primary">Добавить</button>
+    </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "Form",
+    data() {
+        return {
+            formFields: null,
+        }
+    },
+    async created() {
+            await axios.get('https://demo-api.vsdev.space/api/farm/baby/form').then(response => this.formFields = response.data.fields);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/animals.vue
+++ pages/animals.vue
@@ -2,37 +2,17 @@
     <div>
         <h1>Животные</h1>
         <button class="btn btn-primary" @click="isFormVisible = !isFormVisible">Открыть форму</button>
-        <form action="" v-if="isFormVisible">
-            <div class="mb-3">
-                <label  class="form-label">Тип животного</label>
-                <select class="form-control form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                    <option v-model="currentAnimalType">Корова</option>
-                    <option v-model="currentAnimalType">Кролик</option>
-                    <option v-model="currentAnimalType">Овца</option>
-                </select>
+        <Form v-if="isFormVisible"/>
+        <div class="row" v-if="animals">
+            <div class="col-md-6" v-for="(animal,index) in animals" :key="index">
+                <h2>{{animal.name}}</h2>
+                <img v-if="animal.type === 'cow'" src="cow_img.jpg">
+                <img v-if="animal.type === 'rabbit'" src="rabbit_img.jpg">
+                <img v-if="animal.type === 'sheep'" src="sheep_img.jpg">
+                <p><b>Вес: </b>{{ animal.weight }} кг</p>
+                <p><b>Цвет: </b>{{ animal.color }}</p>
+                <p><b>Пол: </b>{{ animal.sex }}</p>
             </div>
-            <div class="mb-3">
-                <label  class="form-label">Укажите количество животных</label>
-                <input v-model="currentAnimalCount" type="number" class="form-control" id="exampleFormControlInput1" placeholder="Количество животных" :min="1">
-            </div>
-            <button  type="button" class="btn btn-primary" @click="addAnimals(currentAnimalType, currentAnimalCount)">Добавить</button>
-        </form>
-        <div class="row">
-            <div class="col-md-6 mt-4">
-                <img src="../static/cow_img.jpg" alt="">
-                <p>Всего коров: {{ cows }}</p>
-            </div>
-
-            <div  class="col-md-6 mt-4">
-                <img src="../static/rabbit_img.jpg" alt="">
-                <p>Всего кроликов: {{ rabbits }}</p>
-            </div>
-
-            <div class="col-md-6 mt-4">
-                <img  src="../static/sheep_img.jpg" alt="">
-                <p>Всего овец: {{ sheeps }}</p>
-            </div>
-
         </div>
     </div>
 </template>
@@ -40,7 +20,6 @@
 <script>
 
 import axios from "axios";
-
 
 export default {
     name: "animals",
@@ -51,33 +30,13 @@ export default {
     data() {
         return {
             isFormVisible: false,
-            currentAnimalType: 'cows',
-            currentAnimalCount: 1,
-            cows: 0,
-            rabbits: 0,
-            sheeps: 0,
-
-            // animalsContent: null,
+            animals: null
         }
     },
-    methods: {
-        addAnimals(animalType,count) {
-            switch (animalType) {
-                case('cows'):
-                    this.cows += count;
-                    break;
-                case('rabbits'):
-                    this.rabbits += count;
-                    break;
-                case('sheeps'):
-                    this.sheeps += count;
-                    break;
-            }
-        }
+    async created() {
+        await axios.get('https://demo-api.vsdev.space/api/farm/baby').then(response => this.animals = response.data);
     }
-    // async created() {
-    //         await axios.get('https://demo-api.vsdev.space/api/farm/home_page').then(response => this.animalsContent = response.data);
-    // }
+
 }
 </script>
 
